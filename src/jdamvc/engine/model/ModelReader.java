ed255a4b96622d7c662b38798e66ce1d3cdf04b0
denkers <kyle.business@inbox.com>
2015-12-09 01:22:02 +1300

    Updated ModelReader, changed all mapping rentention policy to runtime

--- src/jdamvc/engine/model/ModelReader.java
+++ src/jdamvc/engine/model/ModelReader.java
@@ -1,11 +1,19 @@
 
 package jdamvc.engine.model;
 
+import java.lang.annotation.Annotation;
 import java.util.Map;
 
 public class ModelReader
 {
     private static Map<String, Model> models;
+    
+    private static Annotation[] getAnnotationsForModal(Model model)
+    {
+        return model.getClass().getAnnotations();
+    }
+    
+    
     
     public static Model getModel(String tableName)
     {
