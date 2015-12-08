5513167db205caa17535882bce62935d0a0cdf84
denkers <kyle.business@inbox.com>
2015-12-09 02:12:11 +1300

    Added getColumns and getEntity methods in ModelReader

--- src/jdamvc/engine/model/ModelReader.java
+++ src/jdamvc/engine/model/ModelReader.java
@@ -2,7 +2,11 @@
 package jdamvc.engine.model;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
+import jdamvc.engine.core.database.mapping.Attribute;
+import jdamvc.engine.core.database.mapping.Entity;
 
 public class ModelReader
 {
@@ -13,6 +17,32 @@ public class ModelReader
         return model.getClass().getAnnotations();
     }
     
+    public static Entity getEntityName(Model model)
+    {
+        Annotation entity = model.getClass().getAnnotation(Entity.class);
+        if(entity != null) return (Entity) entity;
+        else return null;
+    }
+    
+    public static Map<String, Attribute> getColumns(Model model)
+    {
+        Map<String, Attribute> attrs = new HashMap<>();
+        Field[] fields = model.getClass().getDeclaredFields();
+        for(Field field : fields)
+        {
+            Annotation annotation = field.getDeclaredAnnotation(Attribute.class);
+            if(annotation != null)
+            {
+                Attribute attr = (Attribute) annotation;
+                if(attr.name().equals(""))
+                    attrs.put(field.getName(), attr);
+                else
+                    attrs.put(attr.name(), attr);
+            }
+        }
+        
+        return attrs;
+    }
     
     
     public static Model getModel(String tableName)
