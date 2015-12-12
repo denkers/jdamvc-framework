26a2616c0dc1ed2b124d5b357f68bf6966b8dfd4
denkers <kyle.business@inbox.com>
2015-12-13 01:40:27 +1300

    Updated Routes with initialization of routes and route groups with new routes xml document

--- src/jdamvc/engine/core/Routes.java
+++ src/jdamvc/engine/core/Routes.java
@@ -7,6 +7,11 @@
 package jdamvc.engine.core;
 
 //------------------------------------------
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 //                ROUTES
 //------------------------------------------
 //- Routes define paths in the application
@@ -23,12 +28,41 @@ public class Routes extends Router
         super();
     }
     
+    private static final String ROUTE_TAG       =   "Route";
+    
+    private static final String ROUTE_GROUP_TAG =   "RouteGroup";
+    
+    private void initRoute(Node node)
+    {
+        
+    }
+    
+    private void initRouteGroup(Node node)
+    {
+        
+    }
+    
     //Define the applications routes in here
     //Routes only need to be defined once
     @Override
     protected void initRoutes()
     {
-        add("getErrorPage", "BaseController", "getErrorView", "/error");
-        add("getHome", "MasterController", "getHomeView", "/home");
+        String routePath    =   "application/Routes.xml";
+        Document doc        =   XMLParser.getDocument(routePath);
+        
+        NodeList childNodes     =   doc.getChildNodes();
+        int numNodes            =   childNodes.getLength();
+        
+        for(int i = 0; i < numNodes; i++)
+        {
+            Node node   =   childNodes.item(i);
+            String tag  =   node.getNodeName();
+            
+            if(tag.equalsIgnoreCase(ROUTE_TAG))
+                initRoute(node);
+            
+            else if(tag.equalsIgnoreCase(ROUTE_GROUP_TAG))
+                initRouteGroup(node);
+        }
     }
 }
