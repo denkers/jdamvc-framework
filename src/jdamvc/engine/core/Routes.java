fb5acd13c6a6c50917053d9909bd70d9dc8b2c02
denkers <kyle.business@inbox.com>
2015-12-12 13:42:18 +1300

    Added previous route contents from application to routes in engine

--- src/jdamvc/engine/core/Routes.java
+++ src/jdamvc/engine/core/Routes.java
@@ -1,8 +1,34 @@
+//====================================
+//	Kyle Russell
+//	jdamvc
+//	Routes
+//====================================
 
 package jdamvc.engine.core;
 
+//------------------------------------------
+//                ROUTES
+//------------------------------------------
+//- Routes define paths in the application
+//- Routes map to a controller that handles behaviour
+//- Routes can be accessed by name and location
+//- Paramaters can be passed where necessary
+//- Filters can be used to restrict access to a route
+//- Groups can be defined to cluster similar routes
 
-public class Routes 
+public class Routes extends Router 
 {
+    public Routes()
+    {
+        super();
+    }
     
+    //Define the applications routes in here
+    //Routes only need to be defined once
+    @Override
+    protected void initRoutes()
+    {
+        add("getErrorPage", "BaseController", "getErrorView", "/error");
+        add("getHome", "MasterController", "getHomeView", "/home");
+    }
 }
